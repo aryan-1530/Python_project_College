546c16f87bad59f9b71899485aaddb4b07c01bbd
aryan-1530 <aaryan0939@gmail.com>
2024-09-13 08:53:34 +0530

    Update settings.py

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -115,11 +115,9 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# Define the STATIC_ROOT setting
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Define the STATIC_URL setting
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
